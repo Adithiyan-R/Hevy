eb41772e88aa8656b241338d9b5dcc7a620ebe51
Adithiyan-R <adithiyan13r@gmail.com>
2024-07-03 21:14:00 +0530

    added build cmd

--- server/index.ts
+++ server/index.ts
@@ -18,18 +18,6 @@ app.use(
     })
   );
 
-  app.use((req : customRequest,res : Response,next : NextFunction)=>{
-    res.setHeader('Access-Control-Allow-Credentials', '*')
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    // another common pattern
-    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-    res.setHeader(
-      'Access-Control-Allow-Headers',
-      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-    )
-  })
-
 app.use(bodyParser.json());
 
 interface customRequest extends Request{
